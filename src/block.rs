dfe30d409acfda6d5020829848f0c8d2df9e02db
lvqinghao <lvqinghao@163.com>
2020-09-01 14:10:54 +0800

    first commit

---
+++ src/block.rs
@@ -0,0 +1,149 @@
+
+use crate::{
+	header::Header,
+	transaction::{UnverifiedTransaction},
+};
+use parity_bytes::Bytes;
+use rlp::{Rlp, RlpStream, Decodable, DecoderError, Encodable};
+use ethereum_types::H256;
+
+
+/// A block, encoded as it is on the block chain.
+#[derive(Default, Debug, Clone, PartialEq)]
+pub struct Block {
+	/// The header of this block.
+	pub header: Header,
+	/// The transactions in this block.
+	pub transactions: Vec<UnverifiedTransaction>,
+}
+
+impl Block {
+
+	pub fn new(header: Header, transactions: Vec<UnverifiedTransaction>) -> Self {
+		Block{
+			header,
+			transactions,
+		}
+	}
+}
+
+impl Encodable for Block {
+	/// Get the RLP-encoding of the block with the seal.
+	fn rlp_append(&self, s: &mut RlpStream){
+		s.begin_list(2);
+		s.append(&self.header);
+		s.append_list(&self.transactions);
+	}
+}
+
+impl Decodable for Block {
+	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+		if rlp.as_raw().len() != rlp.payload_info()?.total() {
+			return Err(DecoderError::RlpIsTooBig);
+		}
+		if rlp.item_count()? != 2 {
+			return Err(DecoderError::RlpIncorrectListLen);
+		}
+		Ok(Block {
+			header: rlp.val_at(0)?,
+			transactions: rlp.list_at(1)?,
+		})
+	}
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BlockHashListError {
+	DuplicateHash,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BlockHashList(Vec<H256>);
+
+impl Default for BlockHashList {
+	fn default() -> Self {
+		BlockHashList(vec![])
+	}
+}
+
+impl BlockHashList {
+
+	pub fn push(&mut self,block_hash: H256) -> Result<(),BlockHashListError> {
+		match self.0.iter().filter(|h| **h == block_hash).count() {
+			0 => {
+				self.0.push(block_hash);
+				Ok(())
+			}
+
+			_ => {
+				Err(BlockHashListError::DuplicateHash)
+			}
+		}
+	}
+
+	pub fn push_main_block_hash(&mut self, main_block_hash: H256) {
+		self.0.retain(|h| h != &main_block_hash);
+		self.0.insert(0,main_block_hash);
+	}
+
+	pub fn block_hashes(&self) -> &Vec<H256> {
+		&self.0
+	}
+}
+
+impl Encodable for BlockHashList {
+	/// Get the RLP-encoding of the block with the seal.
+	fn rlp_append(&self, s: &mut RlpStream){
+		s.begin_list(1);
+		s.append_list(&self.0);
+	}
+}
+
+impl Decodable for BlockHashList {
+	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+		if rlp.as_raw().len() != rlp.payload_info()?.total() {
+			return Err(DecoderError::RlpIsTooBig);
+		}
+		if rlp.item_count()? != 1 {
+			return Err(DecoderError::RlpIncorrectListLen);
+		}
+		Ok(BlockHashList(rlp.list_at(0)?))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::str::FromStr;
+
+	#[test]
+	fn block_hash_list_test() {
+		let block_hash1 = H256::from_str("40eb088232727a64c88d5e98d25e7b16ee7e9acc267c25c0088c7d1432745896").unwrap();
+		let block_hash2 = H256::from_str("30eb088232727a64c88d5e98d25e7b16ee7e9acc267c25c0088c7d1432745896").unwrap();
+
+		let mut list = BlockHashList::default();
+		list.push(block_hash1.clone()).unwrap();
+		list.push(block_hash2.clone()).unwrap();
+
+		let list_bytes = list.rlp_bytes();
+		let list2: BlockHashList = rlp::decode(list_bytes.as_slice()).unwrap();
+		assert_eq!(list,list2);
+	}
+
+	#[test]
+	fn block_hash_list_push_main_hash_test() {
+		let block_hash1 = H256::from_str("40eb088232727a64c88d5e98d25e7b16ee7e9acc267c25c0088c7d1432745896").unwrap();
+		let block_hash2 = H256::from_str("30eb088232727a64c88d5e98d25e7b16ee7e9acc267c25c0088c7d1432745896").unwrap();
+		let block_hash3 = H256::from_str("20eb088232727a64c88d5e98d25e7b16ee7e9acc267c25c0088c7d1432745896").unwrap();
+
+		let mut list = BlockHashList::default();
+		list.push(block_hash1.clone()).unwrap();
+		list.push(block_hash2.clone()).unwrap();
+
+		list.push_main_block_hash(block_hash3);
+		assert_eq!(list.block_hashes(),&vec![block_hash3,block_hash1,block_hash2]);
+	}
+
+}
+
+
+
---
+++ src/ipc.rs
@@ -0,0 +1,202 @@
+
+use hex_literal::hex;
+use rlp::{Decodable, DecoderError, Encodable, Rlp, RlpStream};
+use ethereum_types::{H256,U256,Address};
+use parity_bytes::Bytes;
+use rlp_derive::{RlpEncodable, RlpDecodable};
+
+use crate::transaction::{UnverifiedTransaction};
+use crate::header::Header;
+use log::{debug, error};
+use zmq::Socket;
+
+
+/// RLP-Encode( method(string), id(number), param(rlp-encoded-list) );
+#[derive(Default, Debug, Clone, PartialEq)]
+pub struct IpcRequest {
+    pub method: String,
+    pub id: u64,
+    pub params: Vec<u8>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq)]
+pub struct IpcReply {
+    pub id: u64,
+    pub result: Vec<u8>,
+}
+
+impl Encodable for IpcRequest {
+    fn rlp_append(&self, s: &mut RlpStream){
+        s.begin_list(3);
+        s.append(&self.method);
+        s.append(&self.id);
+        s.append(&self.params);
+    }
+}
+
+impl Decodable for IpcRequest {
+    fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+        if rlp.as_raw().len() != rlp.payload_info()?.total() {
+            return Err(DecoderError::RlpIsTooBig);
+        }
+        if rlp.item_count()? != 3 {
+            return Err(DecoderError::RlpIncorrectListLen);
+        }
+        Ok(IpcRequest {
+            method: rlp.val_at(0)?,
+            id: rlp.val_at(1)?,
+            params: rlp.val_at(2)?,
+        })
+    }
+}
+
+impl Encodable for IpcReply {
+    fn rlp_append(&self, s: &mut RlpStream){
+        s.begin_list(2);
+        s.append(&self.id);
+        s.append(&self.result);
+    }
+}
+
+impl Decodable for IpcReply {
+    fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+        if rlp.as_raw().len() != rlp.payload_info()?.total() {
+            return Err(DecoderError::RlpIsTooBig);
+        }
+        if rlp.item_count()? != 2 {
+            return Err(DecoderError::RlpIncorrectListLen);
+        }
+        Ok(IpcReply {
+            id: rlp.val_at(0)?,
+            result: rlp.val_at(1)?,
+        })
+    }
+}
+
+/// method: CreateHeader, Request
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct CreateHeaderReq {
+    pub parent_block_hash: H256,
+    pub author: Address,
+    pub extra_data: Bytes,
+    pub gas_limit: U256,
+    pub difficulty: U256,
+    pub transactions: Vec<UnverifiedTransaction>,
+}
+
+impl CreateHeaderReq {
+    pub fn new(parent_block_hash: H256,
+               author: Address,
+               extra_data: Bytes,
+               gas_limit: U256,
+               difficulty: U256,
+               transactions: Vec<UnverifiedTransaction>) -> Self{
+        CreateHeaderReq {
+            parent_block_hash,
+            author,
+            extra_data,
+            gas_limit,
+            difficulty,
+            transactions,
+        }
+    }
+}
+
+/// method: CreateHeader, Response
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct CreateHeaderResp(pub Header);
+
+/// method: LatestBlocks, Request
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct LatestBlocksReq(pub u64);
+/// method: LatestBlocks, Response
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct LatestBlocksResp(pub Vec<Header>);
+
+/// method: ApplyBlock, Request
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct ApplyBlockReq(pub Header,pub Vec<UnverifiedTransaction>);
+/// method: ApplyBlock, Response
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct ApplyBlockResp(pub bool);
+
+/// method: AccountInfo, Request
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct AccountInfoReq(pub Address);
+/// method: ApplyBlock, Response
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct AccountInfoResp(pub U256, pub U256);
+
+/// method: TxHashList, Request
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct TxHashListReq(pub H256);
+/// method: TxHashList, Response
+#[derive(Default, Debug, Clone, PartialEq,RlpEncodable, RlpDecodable)]
+pub struct TxHashListResp(pub Vec<H256>);
+
+pub fn query_account_info(socket: &Socket, account: &Address) -> (U256, U256) {
+    let request = IpcRequest {
+        method: "AccountInfo".into(),
+        id: 1,
+        params: rlp::encode(&AccountInfoReq(*account)),
+    };
+
+    let reply = request_chain(socket, request);
+    let resp: AccountInfoResp = rlp::decode(&reply.result).unwrap();
+
+    let (nonce, balance) = (resp.0, resp.1);
+    debug!("query accout info: {}, {}, {}", account, nonce, balance);
+    (nonce, balance)
+}
+
+
+pub fn request_chain(socket: &Socket, request: IpcRequest) -> IpcReply {
+    socket.send(rlp::encode(&request), 0).unwrap();
+    let mut received_parts = socket.recv_multipart(0).unwrap();
+    let msg_bytes = received_parts.pop().unwrap();
+    rlp::decode(&msg_bytes).unwrap()
+}
+
+pub fn query_last_block(socket: &Socket) -> Header {
+    let request = IpcRequest {
+        method: "LatestBlocks".into(),
+        id: 1,
+        params: rlp::encode(&LatestBlocksReq(1)),
+    };
+
+    let reply = request_chain(&socket, request);
+    let resp: LatestBlocksResp = rlp::decode(&reply.result).unwrap();
+
+    let last_block_header = resp.0.get(0).unwrap().clone();
+    last_block_header
+}
+
+pub fn query_latest_blocks(socket: &Socket, n: u64) -> Vec<Header> {
+    let request = IpcRequest {
+        method: "LatestBlocks".into(),
+        id: 1,
+        params: rlp::encode(&LatestBlocksReq(n)),
+    };
+
+    let reply = request_chain(&socket, request);
+    let resp: LatestBlocksResp = rlp::decode(&reply.result).unwrap();
+
+    let headers_vec = resp.0;
+    headers_vec
+}
+
+pub fn query_tx_hash_list(socket: &Socket, block_hash: H256) -> Vec<H256> {
+    let request = IpcRequest {
+        method: "TxHashList".into(),
+        id: 1,
+        params: rlp::encode(&TxHashListReq(block_hash)),
+    };
+
+    let reply = request_chain(&socket, request);
+    let resp: TxHashListResp = rlp::decode(&reply.result).unwrap();
+
+    let hash_list = resp.0;
+    hash_list
+}
+
+
